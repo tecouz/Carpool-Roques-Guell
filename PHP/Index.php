9fd60150cfdc0731432193f779450803540bf497
Vincent Guell <vguell57@gmail.com>
2024-02-28 15:52:34 +0100

    Co-authored-by: Flo <Flo5797@users.noreply.github.com>

---
+++ PHP/Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+    <title>Carpool</title>
+</head>
+
+<body>
+    <header>
+        <h1>Bienvenu sur Carpool IFA-MNS</h1>
+        <a href=HTML\inscription.html>inscription</a>
+        <a href=HTML\connexion.html>connexion</a>
+
+    </header>
+</body>
+
+</html>
