99077b934acd8bf5a60af188eaa13ae9923f1fa1
Vincent Guell <vguell57@gmail.com>
2024-03-19 16:59:22 +0100

    header finis, début page connexion

---
+++ php/connexion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="login">
+            <a href="">inscription</a>
+        </div>
+        <h1>Carpool IFA-MNS</h1>
+    </header>
+
+    <div class="container"></div>
+</body>
+
+</html>
