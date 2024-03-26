401005a6e8534f75be8dbb1685a325a89f12e01a
Florian Roques <superflorianroques@gmail.com>
2024-03-26 11:55:02 +0100

    commit 26/03

---
+++ php/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
+    <title>Carpool</title>
+</head>
+
+<body>
+    <header>
+        <div class="login">
+            <p>
+                <a href="">connexion</a>
+                /
+                <a href="">inscription</a>
+            </p>
+        </div>
+        <h1>Bienvenu sur Carpool IFA-MNS</h1>
+    </header>
+    <div class="container">
+
+        <div class="Logo">
+            <img src='../image/Logo_Carpool_IFA_MNS'></img>
+        </div>
+        <div class="button">plus d'info</div>
+
+    </div>
+    <footer>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti voluptas, iusto ullam excepturi aperiam suscipit itaque molestiae ratione sequi cumque neque enim corrupti voluptate odio similique quam quia, perferendis eligendi.</p>
+    </footer>
+</body>
+
+
+</html>
---
+++ php/info.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../CSS/style.css" />
+  <link rel="stylesheet" type="text/css" href="../CSS/info.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet" />
+
+  <title>Informations</title>
+</head>
+<header>
+  <h1> Bienvenu sur carpool IFA-MNS</h1>
+</header>
+
+<body>
+  <img src="../Image/Logo_Carpool_IFA_MNS.png">
+  <p>
+    Bienvenue sur Carpool IFA-MNS, une application web qui va vous permettre de faciliter l’accès a votre lieu de Formation/Travail !
+  </p>
+  <img src"">
+  <img src"">
+  <p>Vous souhaitez faire une action pour preservez nôtre planète Carpool IFA-MNS va vous y aidez en effet le covoiturage dispose d’avantage a la fois personnel et pour autrui : </p>
+</body>
+
+</html>
