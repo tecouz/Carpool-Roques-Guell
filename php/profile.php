b8851210bc3e77f5bc44f49b691d71f7589780fa
Vincent Guell <vguell57@gmail.com>
2024-03-26 16:53:55 +0100

    profile.php debut html

--- php/profile.php
+++ php/profile.php
@@ -5,10 +5,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/profile.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&display=swap" rel="stylesheet">
 </head>
 
 <body>
+    <header>
+        <h1>Carpool IFA-MNS</h1>
+    </header>
 
+    <div class="container">
+        <div class="name">
+            <h2></h2>
+        </div>
+        <div class="pdp">
+            <img src="../image/default.png" alt="">
+        </div>
+        <div class="bio">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur adipisci sunt nulla iste tempora
+                eligendi recusandae quod tempore doloribus quos! Numquam amet ipsam id consequatur aliquid magni
+                mollitia. Enim, iusto?</p>
+        </div>
+        <div class="preference">
+
+        </div>
+        <div class="commentaire">
+
+        </div>
+    </div>
 </body>
 
 </html>
